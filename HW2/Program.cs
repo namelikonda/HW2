7feaf3137625b1a35a397ab5c0f9020e72f04c01
namelikonda <54454186+namelikonda@users.noreply.github.com>
2019-08-27 20:34:44 -0400

    Add project files.

---
+++ HW2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HW2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Press Any Key to COntinue..");
+            Console.ReadKey();
+        }
+    }
+}
